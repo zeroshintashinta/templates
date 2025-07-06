6f27261f2b09aaf1047b94ce92385d9935ad98ea
zeroshintashinta <153372948+zeroshintashinta@users.noreply.github.com>
2025-07-06 12:49:14 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+print("Start")
+print("Buat kopi")
+print("Masak air dan nyalakan kompor")
+print("Siapkan gelas")
+print("Ambil gula satu sendok teh")
+print("Matikan kompor")
+print("Tuang dan aduk")
+
+# Pilih bubuk
+bubuk = "kopi"  # bisa diubah jadi "kopi","jahe", "teh", atau "susu"
+
+if bubuk == "teh":
+    print("Ambil bubuk teh")
+elif bubuk == "jahe":
+    print("Ambil bubuk jahe")
+elif bubuk == "susu":
+    print("Ambil bubuk susu")
+elif bubuk == "kopi":
+    print("Ambil bubuk kopi")
+
+# Pakai es atau tidak
+pakai_es = True
+if pakai_es:
+    print("Pakai es batu")
+else:
+    print("Tidak pakai es batu")
+
+print("Sajikan")
+
+print("Selesai")
+print("Terima kasih telah membuat kopi")
